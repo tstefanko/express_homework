aed75c0689b7cfd0e1a3a9eb158b503fa0f622fc
Ivan Halupka <ivan.halupka@dazn.com>
2021-03-25 12:05:45 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const library = [
+    {
+    "title": "Robinson Crusoe",
+    "author": "Daniel Defoe",
+    "pages": 300,
+    "tags": [
+      "adventure",
+      "history"
+    ],
+    "id": 0
+  },
+  {
+    "title": "The Unbearable Lightness of Being",
+    "author": "Milan Kundera",
+    "pages": 250,
+    "tags": [
+      "philosophical",
+      "novel"
+    ],
+    "id": 0
+  },
+{
+  "title": "Nausea",
+  "author": "Jean-Paul Sartre",
+  "pages": 120,
+  "tags": [
+    "philosophical",
+    "existentialism",
+    "novel"
+  ],
+  "id": 0
+},
+]
+
